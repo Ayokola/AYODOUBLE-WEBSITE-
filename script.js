46996fce4cd6b466f01fd435ca86b9137920ad8d
Ayokola <103223255+Ayokola@users.noreply.github.com>
2026-07-26 01:13:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
 function toggleMenu() {
     document.getElementById("nav-links").classList.toggle("active");
 }
+// ===============================
+// SCROLL ANIMATION
+// ===============================
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
