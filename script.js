d3ec851a4729fc2f7d90e9bb23a0849ee7aaff27
Ayokola <103223255+Ayokola@users.noreply.github.com>
2026-07-28 08:53:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -477,3 +477,18 @@ doc.text("Thank you for choosing AYODOUBLE.", 20, y + 48);
 };
 
 }
+// ===============================
+// STICKY HEADER
+// ===============================
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 80) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+
+});
